bd9610a115c1ecd9367fbbbc87294bcdf78ce04c
Simon Weber <simon@simonmweber.com>
2016-08-28 16:46:59 -0400

    note gevent/command limitation

--- gchatautorespond/apps/autorespond/management/commands/runworker.py
+++ gchatautorespond/apps/autorespond/management/commands/runworker.py
@@ -1,3 +1,6 @@
+# Note that this won't work with gevent, since you need to patch it earlier.
+# Just switch to a normal script with manual setup for that.
+
 from django.core.management.base import BaseCommand
 
 from gchatautorespond.lib.chatworker import Worker
